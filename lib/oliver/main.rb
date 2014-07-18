b1e3a8187ca538fff32dbda4d2b34809f5528172
Josh <joshception@icloud.com>
2014-07-18 01:01:18 -0700

    ok, I keep screwing things up accidentally

--- lib/oliver/main.rb
+++ lib/oliver/main.rb
@@ -1,9 +1,8 @@
 # Main oliver file
 def run_main
-    body = File.read(Name::OLIVER.chomp)
-    final = YAML.load("---\n#{body}\n---")
-    final["repos"].each do |repo|
-      %x(git clone git://github.com/#{repo} --quiet)
-    end
+  body = File.read(Name::OLIVER.chomp)
+  final = YAML.load("---\n#{body}\n---")
+  final["repos"].each do |repo|
+    %x(git clone git://github.com/#{repo} --quiet)
   end
 end
