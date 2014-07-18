a0bfa85cf29cb7f42dce0ce2ae15be3bbefd3ca8
Josh <joshception@icloud.com>
2014-07-18 00:26:41 -0700

    maybe

--- lib/oliver/main.rb
+++ lib/oliver/main.rb
@@ -1,8 +1,12 @@
 # Main oliver file
 def run_main
-  body = File.read(Name::OLIVER.chomp)
-  final = YAML.load("---\n#{body}\n---")
-  final["repos"].each do |repo|
-    %x(git clone git://github.com/#{repo} --quiet)
+  if ARGV[1].downcase == 'init'
+    puts "I'll implement this later"
+  else
+    body = File.read(Name::OLIVER.chomp)
+    final = YAML.load("---\n#{body}\n---")
+    final["repos"].each do |repo|
+      %x(git clone git://github.com/#{repo} --quiet)
+    end
   end
 end
