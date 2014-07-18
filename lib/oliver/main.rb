eb9f7f09c86d82e3f8f4b3124de6a3bf2ff9b1ec
Josh <joshception@icloud.com>
2014-07-18 01:55:13 -0700

    added and fixed a couple of things

--- lib/oliver/main.rb
+++ lib/oliver/main.rb
@@ -1,8 +1,26 @@
 # Main oliver file
 def run_main
+
+  # Back up your files, man
+  if ! File.directory?('backup')
+    Dir.mkdir 'backup'
+  end
+
+  # The buggiest code in the world (that's probably totally untrue)
   body = File.read(Name::OLIVER.chomp)
   final = YAML.load("---\n#{body}\n---")
-  final["repos"].each do |repo|
-    %x(git clone git://github.com/#{repo} --quiet)
+  final["repos"].each do |url|
+
+    # Split the url in half
+    splitted = url.split '/'
+    username = splitted[0]
+    repo = splitted[1]
+
+    if ! File.directory?(repo)
+      %x(git clone git://github.com/#{url} --quiet)
+    else
+      puts "#{Rainbow(repo).red}/ already exists."
+    end
+
   end
 end
