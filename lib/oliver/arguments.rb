74a440ac762378f35ef197c32194ede0af08f368
Josh <joshception@icloud.com>
2014-07-26 23:46:20 -0700

    hopefully things are better now

--- lib/oliver/arguments.rb
+++ lib/oliver/arguments.rb
@@ -1,3 +1,37 @@
+def argument?(argument_number)
+  help if ARGV[argument_number].nil?
+end
+
+def init
+  if !File.file?(Name::OLIVER)
+    File.open(Name::OLIVER, 'w') do |file|
+      file.write("repos:\n")
+      file.write("   - \"trommel/oliver\"\n")
+      puts "#{Rainbow(Name::OLIVER).green} has been created."
+    end
+  else
+    puts "#{Rainbow(Name::OLIVER).red} already exists."
+  end
+end
+
+def list
+  puts "list repos currently in the Olivefile"
+end
+
+def update
+  puts "pull updates from each tracked repo on the Olivefile"
+end
+
+def add
+  argument?(1)
+  puts "added #{Rainbow(ARGV[1]).green} to #{Rainbow('Olivefile').green}."
+end
+
+def remove
+  argument?(1)
+  puts "removed #{Rainbow(ARGV[1]).red} from #{Rainbow('Olivefile').green}."
+end
+
 # Different arguments
 # (Make this into a case statement asap)
 # (It'll be way easier and cleaner to work with later)
@@ -6,27 +40,17 @@ def different_arguments
   when 'install'
     run_main
   when 'init'
-    if !File.file?(Name::OLIVER)
-      File.open(Name::OLIVER, 'w') do |file|
-        file.write("repos:\n")
-        file.write("   - \"trommel/oliver\"\n")
-        puts "#{Rainbow(Name::OLIVER).green} has been created."
-      end
-    else
-      puts "#{Rainbow(Name::OLIVER).red} already exists."
-    end
-    exit
+    init
   when 'list'
-    puts "list repos currently in the Olivefile"
+    list
   when 'update'
-    puts "pull updates from each tracked repo on the Olivefile"
+    update
   when 'add'
-    puts "added #{ARGV[1]} to Olivefile"
+    add
   when 'remove'
-    puts "removed #{ARGV[1]} from Olivefile"
+    remove
   when '-v' || '--version'
     puts "#{Rainbow('oliver').red} #{Rainbow("v#{Oliver::VERSION}").green}"
-    exit
   when '-h' || '--help'
    help
  else
--- lib/oliver/main.rb
+++ lib/oliver/main.rb
@@ -1,6 +1,9 @@
 # Main oliver file
 def run_main
-  help if !File.file? Name::OLIVER
+  if !File.file? Name::OLIVER
+    puts "#{Rainbow('Olivefile').red} does not exist."
+    init
+  end
 
   # Back up your files, man
   if !File.directory?('.backup')
@@ -11,7 +14,18 @@ def run_main
   # (that's probably totally untrue)
   body = File.read(Name::OLIVER.chomp)
   final = YAML.load("---\n#{body}\n---")
+
+  # Fix this ASAP
+  if final["repos"].nil?
+    message = "This will normally return a bug, so I'm just not going to do it.
+You have an empty repos list, though, and that's what's causing this bug.
+Try adding something to the list for the time being."
+    puts Rainbow(message).red
+    exit
+  end
+
   listed_repos = []
+
   final["repos"].each do |what|
     listed_repos.push(what)
   end
@@ -42,7 +56,13 @@ def run_main
     # in `Name::OLIVER`, move it to .backup
     current_repos.each do |directory_thing|
       if !listed_repos.to_s.include? directory_thing
-        %x(mv #{directory_thing} .backup)
+        if !File.directory?(directory_thing)
+          %x(mv #{directory_thing} .backup)
+        else
+          backup_color = Rainbow('.backup/').red
+          repo_color = Rainbow(repo).red
+          puts "There was an error moving #{repo_color}/ to #{backup_color}"
+        end
       end
     end
 
