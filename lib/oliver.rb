388c40884247d56b497c8e930d5b0904807e8a9b
Josh <joshception@icloud.com>
2014-08-21 15:42:02 -0700

    shit, oops

--- lib/oliver.rb
+++ lib/oliver.rb
@@ -1,10 +1,14 @@
 # Require files
 %w(rainbow YAML).each { |file| require file }
 require_relative 'oliver/version'
+require_relative 'oliver/argument_files/version'
 
 # if the user doesn't want to use rainbow
 # just disable it completely
 Rainbow.enabled = false if ARGV[-1] == 'colour=off'
+
+# Provide help if no arguments are specified
+help if ARGV.nil?
 
 # Arguments
 require_relative 'oliver/arguments'
