0b734f5f44e1eee3032187065fc7b761ae55006a
Josh <joshception@icloud.com>
2014-08-21 00:28:18 -0700

    added an options hash and stuff

--- lib/oliver/arguments.rb
+++ lib/oliver/arguments.rb
@@ -1,29 +1,49 @@
 # Require files
-%w(oliver_file_name file_manager).each { |file| require_relative file }
+files = %w(add init install list remove update help)
+files.each do |file|
+  require_relative "argument_files/#{file}"
+end
 
-# `argument?` returns help if the specified argument is nonexistant
-def argument?(argument_number) help if ARGV[argument_number].nil? end
+# Default options
+options = {
+  :verbose => true,
+  :silent  => false
+}
+
+# `argument?` returns help if the
+# specified argument is nonexistant
+def argument?(argument_number)
+  help if ARGV[argument_number].nil?
+end
 
 # Different arguments
-case ARGV[0]
-when 'install'
-  require_relative 'argument_files/install'
-when 'init'
-  require_relative 'argument_files/init'
-when 'list'
-  require_relative 'argument_files/list'
-when 'update'
-  require_relative 'argument_files/update'
-when 'add'
-  require_relative 'argument_files/add'
-when 'remove'
-  require_relative 'argument_files/remove'
-when 'help'
-  require_relative 'argument_files/help'
-  help
-when '-v' || '--version'
-  puts "#{Rainbow('oliver').red} #{Rainbow("v#{Oliver::VERSION}").green}"
+def arguments
+  case ARGV[0].downcase
+  when 'install'
+    file_manager
+    install
+  when 'init'
+    file_manager
+    init
+  when 'list'
+    file_manager
+    list
+  when 'update'
+    file_manager
+    update
+  when 'add'
+    file_manager
+    add
+  when 'remove'
+    file_manager
+    remove
+  when 'help'
+    help
+  when '-v' || '--version'
+    puts "#{Rainbow('oliver').red} #{Rainbow("v#{Oliver::VERSION}").green}"
+    exit
+ else
+    puts "#{Rainbow('Error').underline.red}: Unknown argument: #{ARGV[0]}"
+ end
   exit
-else
-  puts "#{Rainbow('Error').underline.red}: Unknown argument: #{ARGV[0]}"
 end
