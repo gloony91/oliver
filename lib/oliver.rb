7b79fafc81959453b2f03ec7c46d119bd655e71f
Josh <joshception@icloud.com>
2014-08-21 15:45:55 -0700

    FINALLY. I AM SO SORRY :/

--- lib/oliver.rb
+++ lib/oliver.rb
@@ -3,12 +3,12 @@
 require_relative 'oliver/version'
 require_relative 'oliver/argument_files/help'
 
+# Provide help if no arguments are specified
+help && exit if ARGV[0].nil?
+
 # if the user doesn't want to use rainbow
 # just disable it completely
 Rainbow.enabled = false if ARGV[-1] == 'colour=off'
-
-# Provide help if no arguments are specified
-help if ARGV.nil?
 
 # Arguments
 require_relative 'oliver/arguments'
