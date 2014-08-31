aa6db9d2c9ecc83f77b8b9d734f32ed1cfc3011d
Josh <joshception@icloud.com>
2014-08-31 15:04:32 -0700

    fixed list.rb up

--- lib/oliver/argument_files/list.rb
+++ lib/oliver/argument_files/list.rb
@@ -1,12 +1,13 @@
 counter = 0
-$yaml['repos'].each do |thing|
-  repo = thing.split('/')[1] # might as well discard the username
-  if File.directory?(repo)
-    print Rainbow(repo).green
-  else
-    print Rainbow(repo).red
+$yaml['repos'].map do |username, repos|
+  repos.each do |repo|
+    if File.directory?(repo)
+      print Rainbow(repo).green
+    else
+      print Rainbow(repo).red
+    end
+    print "\t" if counter % 4 == 0
+    counter += 1
   end
-  print "\t" if counter % 4 == 0
-  counter += 1
 end
 puts
