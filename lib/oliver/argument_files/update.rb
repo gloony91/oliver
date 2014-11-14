7f3ca5920786c300006f4c851ce6c944b628e35d
Josh <joshception@icloud.com>
2014-11-13 21:10:22 -0800

    no no nono

--- lib/oliver/argument_files/update.rb
+++ lib/oliver/argument_files/update.rb
@@ -1,6 +1,6 @@
 require_relative '../oliver_file_name'
 require 'git'
-puts "TODO: pull updates from each tracked repo on the #{Name::OLIVER}"
+# puts "TODO: pull updates from each tracked repo on the #{Name::OLIVER}"
 dirs = Dir.glob('*').select { |f| File.directory? f }
 
 dirs.each do |dir|
