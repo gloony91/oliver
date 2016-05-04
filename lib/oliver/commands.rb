3299c2cb5368f06216139b04786815ab08e92e09
Josh Trommel <joshtrommel@gmail.com>
2016-05-03 18:09:51 -0700

    Help kind of

--- lib/oliver/commands.rb
+++ lib/oliver/commands.rb
@@ -1,11 +1,20 @@
+require 'json'
+
 module Commands
 	module_function
+
 	def help
-		file = File.read('help.json')
-		help = JSON.parse(file)
-		puts 'usage: olive [command] [args]'
-		help.map { |key, value| puts "#{key}\t#{value}"}
+		file = File.read('lib/oliver/commands.json')
+		commands = JSON.parse(file)
+		text = <<-EOS
+		Usage:
+		#{'oliver'.colorize(:blue)} <command> [<args>]
+		Commands:
+		EOS
+		help.map { |key, value| text.push("#{key}\t#{value}") }
+		puts help
 	end
+
 	def init; end
 	def install; end
 	def list; end
